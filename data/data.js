28f7e13e533fe0b8aae3901cee8c224a07aa2a20
Sudip Das <root-sudip@users.noreply.github.com>
2020-06-04 15:43:24 +0530

    Update data.js
    Update publications

--- data/data.js
+++ data/data.js
@@ -26,7 +26,9 @@ var obj = {
         "<font color='#98afc7'>Sudip Das</font>, Partha Sarathi Mukherjee and Ujjwal Bhattacharya. Seek and You Will Find: A New Optimized Framework for Efficient Detection of Pedestrian. <font color='#98afc7'>(ICVGIP 2018)</font>. [<a href='https://128.84.21.199/pdf/1912.10241.pdf'>Paper</a>]",
         "Perla Sai Raj Kishore<sub>*</sub>, <font color='#98afc7'>Sudip Das</font><sub>*</sub>, Partha Sarathi Mukherjee and Ujjwal Bhattacharya. ClueNet : A Deep Framework for Occluded Pedestrian Pose Estimation. <font color='#98afc7'>(BMVC 2019)</font>. [<a href='https://bmvc2019.org/wp-content/uploads/papers/0466-paper.pdf'>Paper</a>] [*equal contributions]",
         "<font color='#98afc7'>Sudip Das</font>, Perla Sai Raj Kishore and Ujjwal Bhattacharya. An End-to-End Framework for Unsupervised Pose Estimation of Occluded Pedestrians. <font color='#98afc7'>(Under review)</font>. [<a href='https://arxiv.org/pdf/2002.06429.pdf'>Paper</a>]",
-        "Kinjal Dasgupta, <font color='#98afc7'>Sudip Das</font> and Ujjwal Bhattacharya. Scale-Invariant Multi-Oriented Text Detection in Wild Scene Images. <font color='#98afc7'>(Under review)</font>. [<a href='https://arxiv.org/pdf/2002.06423.pdf'>Paper</a>]"
+        "Kinjal Dasgupta, <font color='#98afc7'>Sudip Das</font> and Ujjwal Bhattacharya. Scale-Invariant Multi-Oriented Text Detection in Wild Scene Images. <font color='#98afc7'>(Under review)</font>. [<a href='https://arxiv.org/pdf/2002.06423.pdf'>Paper</a>]",
+        "Subhrajyoti Dasgupta, <font color='#98afc7'>Sudip Das</font> and Ujjwal Bhattacharya. CardioGAN: An Attention-based Generative Adversarial Network for Generation of Electrocardiograms. <font color='#98afc7'>(Under review)</font>. [<a href='https://arxiv.org/pdf/2002.06423.pdf'>Paper</a>]"
+        "Kinjal Dasgupta, <font color='#98afc7'>Sudip Das</font> and Ujjwal Bhattacharya. Stratified Multi-Task Learning for Robust Spotting of Scene Texts. <font color='#98afc7'>(Under review)</font>. [<a href='https://arxiv.org/pdf/2002.06423.pdf'>Paper</a>]"
     ],
     "mails": [
         "d.sudip47@gmail.com",
